6243de0e0e7d217da63866fa789946db72d79726
Mateusz Stulczewski <mstulczewski@gmail.com>
2026-04-02 17:06:36 +0200

    test: parse weather json

--- test/desktop/test_network/test_network.cpp
+++ test/desktop/test_network/test_network.cpp
@@ -1,9 +1,11 @@
 #include <unity.h>
 
 #include "test_retry_until_success.h"
+#include "test_parse_weather_json.h"
 
 int main() {
   UNITY_BEGIN();
   test_retry_until_success();
+  test_parse_weather_json();
   return UNITY_END();
 }
